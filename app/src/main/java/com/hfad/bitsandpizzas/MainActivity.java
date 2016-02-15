fbb52cabb0e2725d41f6eb2114573e8526e4f60b
AM1A <boldyhappy123@gmail.com>
2016-02-15 07:20:33 +0530

    first commit

---
+++ app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -0,0 +1,34 @@
+package com.hfad.bitsandpizzas;
+
+import android.app.Activity;
+import android.view.Menu;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.content.Intent;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                Intent intent =  new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
